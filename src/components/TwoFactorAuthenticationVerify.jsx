8e987a48b70ec56c2beb4937749bfe87baed2e3f
Reihaneh <baghishani.r@gmail.com>
2023-05-25 00:03:46 +0200

    fix 2fa scenario

--- src/components/TwoFactorAuthenticationVerify.jsx
+++ src/components/TwoFactorAuthenticationVerify.jsx
@@ -43,54 +43,57 @@ export const TwoFactorAuthenticationVerify = () => {
                 setMessage({
                     type: 'warning',
                     text: "Invalid code"
-                })
+                });
+                return;
             }
+
+            navigate("/");
         } catch (error) {
             console.error(error);
         }
     };
 
     return (
-        <>
-            <h1 className="h4 text-center mb-5">Enable Two Factor Authentication</h1>
-            <Row className="justify-content-center text-center">
-                <Col xs="12" md="6" lg="3">
-                    <Form onSubmit={onSubmit}>
-                        {isLoading && (
-                            <Ratio className="img-thumbnail mb-4 bg-light text-muted">
-                                <div className="d-flex align-items-center justify-content-center">Loading</div>
-                            </Ratio>
-                        )}
-                        {!isLoading && <Image src={qr} className="w-100 mb-4 p-3" thumbnail />}
-                        <p>
-                            Scan this QR with <b>Google Authenticator</b> app and enter the given 6-digit code below:
-                        </p>
-                            {message && <Alert variant={message.type}>{message.text}</Alert>}
+      <>
+          <h1 className="h4 text-center mb-5">Enable Two Factor Authentication</h1>
+          <Row className="justify-content-center text-center">
+              <Col xs="12" md="6" lg="3">
+                  <Form onSubmit={onSubmit}>
+                      {isLoading && (
+                        <Ratio className="img-thumbnail mb-4 bg-light text-muted">
+                            <div className="d-flex align-items-center justify-content-center">Loading</div>
+                        </Ratio>
+                      )}
+                      {!isLoading && <Image src={qr} className="w-100 mb-4 p-3" thumbnail />}
+                      <p>
+                          Scan this QR with <b>Google Authenticator</b> app and enter the given 6-digit code below:
+                      </p>
+                      {message && <Alert variant={message.type}>{message.text}</Alert>}
 
-                        <Form.Group className="mb-3">
-                            <Form.Control
-                                value={code}
-                                onChange={(ev) => setCode(ev.target.value)}
-                                maxLength={6}
-                                type="text"
-                                size="lg"
-                                placeholder="XXXXXX"
-                                className="text-center"
-                                autoFocus
-                                required
-                            />
-                        </Form.Group>
+                      <Form.Group className="mb-3">
+                          <Form.Control
+                            value={code}
+                            onChange={(ev) => setCode(ev.target.value)}
+                            maxLength={6}
+                            type="text"
+                            size="lg"
+                            placeholder="XXXXXX"
+                            className="text-center"
+                            autoFocus
+                            required
+                          />
+                      </Form.Group>
 
-                        <Button type="submit" size="lg" className="w-100 mb-5" disabled={code.length < 6}>
-                            Submit
-                        </Button>
+                      <Button type="submit" size="lg" className="w-100 mb-5" disabled={code.length < 6}>
+                          Submit
+                      </Button>
 
-                        <Link to="/" className="btn btn-light w-100">
-                            Skip
-                        </Link>
-                    </Form>
-                </Col>
-            </Row>
-        </>
+                      <Link to="/" className="btn btn-light w-100">
+                          Skip
+                      </Link>
+                  </Form>
+              </Col>
+          </Row>
+      </>
     );
 };
