2c3b9f117379b323bce6282c473d0e8d77baba40
Reihaneh <baghishani.r@gmail.com>
2023-05-24 23:43:14 +0200

    change port

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: 3000,
   server: {
+    port: 3000,
     proxy: {
       '/api': 'http://localhost:8080'
     }
